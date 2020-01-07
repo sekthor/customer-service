a5aa52c529c378807684ddf89d7841227c3df6f1
sekthor <sysdroyed@gmail.com>
2020-01-07 20:55:42 +0100

    customer added

--- src/main/java/ch/sekthor/customerservice/models/Customer.java
+++ src/main/java/ch/sekthor/customerservice/models/Customer.java
@@ -1,4 +1,37 @@
 package ch.sekthor.customerservice.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String firstName, lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
