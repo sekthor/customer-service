40326e4da761ca0450eaac4c191ee46499c679bc
sekthor <sysdroyed@gmail.com>
2020-01-07 21:06:12 +0100

    customer repository + mysql config added

---
+++ src/main/java/ch/sekthor/customerservice/persitence/CustomerRepository.java
@@ -0,0 +1,9 @@
+package ch.sekthor.customerservice.persitence;
+
+import ch.sekthor.customerservice.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
